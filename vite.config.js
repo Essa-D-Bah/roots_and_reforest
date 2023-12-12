40533087741232a803e35fba2546523cc632aec8
= <=>
2023-12-12 05:41:48 +0000

    fix config 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/roots_and_reforest"
+  base:"/roots_and_reforest/"
 })
 
 
