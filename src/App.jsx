cf69233a17b3c0f471bb5a05c0ef62febc71c971
= <=>
2024-01-22 13:47:46 +0000

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,17 @@ function App() {
       <Navbar />
       <Hero />
       <Intro />
-      <div className="px-4 md:px-8 lg:px-20 ">  
+      <div className="px-4 md:px-8 lg:px-20 ">
         <TourExperience />
       </div>
       <div className="bg-green-500 pt-4 pb-16 px-4 md:px-8 lg:px-20">
-        <OurMission />   
+        <OurMission />
+      </div>
+      <div className="px-4 md:px-8 lg:px-20 border-b pb-12 border-green-500">
+        <Contact />
       </div>
       <div className="px-4 md:px-8 lg:px-20">
-      <Contact />
-      <Teams />
+        <Teams />
       </div>
 
       <Footer />
